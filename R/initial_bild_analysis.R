912245ad6cd1858be28e6444631e66b48c7520f9
John Wilshire <john.h.wilshire@gmail.com>
2015-12-14 09:57:44 +1100

    some clean up, there is an issue with random intercepts...

--- R/initial_bild_analysis.R
+++ R/initial_bild_analysis.R
@@ -1,34 +1,11 @@
 #load the dataset and bild library
 library(bild)
-ball <- read.csv("data/basketball_shots_curry.csv", sep = "|")
 
-### Preparation 
-# remove duplicates rows
-ball <- unique(ball)
 
-# change makes shot to numeric vector 1 = TRUE, 0 = FALSE
-ball$makes_shot <- as.numeric(ball$makes_shot) - 1
+### functions 
 
-# convert the time and dates to comfy POSIX 
-ball$date <-  lapply(ball$date,toString)
-ball$date <- strptime(ball$date,"%B %d, %Y")
-
-# there is probably a better way to do this
-# a player should not be able to have score events twice in a second so worrying about the 
-# tenth of a second isnt really a good idea imho
-ball$time <-  lapply(ball$time,toString)
-ball$time <- strptime(ball$time,"%M:%S")
-ball <- ball[with(ball, order(time, decreasing = TRUE)),]
-ball <- ball[with(ball, order(date, quater)),]
-
-curry <- ball[ball$player.player_id == "curryst01",]
-
-### Bild
-
-# we will call this a lot so I made a function for it,
 # this way we can easily add / change covariates
 # and reevaluate models
-
 bild_model <- function(dataset, dependance, summary = FALSE, time = "shot_age"){
   model <- bild(makes_shot ~ shot_age + quater + score_diff + distance,
                data = dataset,
@@ -41,6 +18,8 @@ bild_model <- function(dataset, dependance, summary = FALSE, time = "shot_age"){
   }
   return(model)
 }
+
+
 
 # shot age calculates the 'age' of a shot based off of the id
 get_shot_age <- function (data) {
@@ -59,21 +38,42 @@ get_shot_age <- function (data) {
   return(shot_age)
 }
 
+
+ball <- read.csv("data/basketball_shots_curry.csv", sep = "|")
+ball <- unique(ball)
+
+# change makes shot to numeric vector 1 = TRUE, 0 = FALSE
+ball$makes_shot <- as.numeric(ball$makes_shot) - 1
+
+# convert the time and dates to comfy POSIX 
+ball$date <-  lapply(ball$date, toString)
+ball$date <- strptime(ball$date,"%B %d, %Y")
+
+
+# there should not be two different score 
+# events within a tenth of a second
+ball$time <-  lapply(ball$time, toString)
+ball$time <- strptime(ball$time, "%M:%S")
+ball <- ball[with(ball, order(time, decreasing = TRUE)),]
+ball <- ball[with(ball, order(date, quater)),]
+
+curry <- ball[ball$player.player_id == "curryst01",]
 ### Model 1: 
 # we only have 1 id'd person only curry
 # only with him for now
 
 curry_3ptr_1 <- curry[curry$event_type == "3-pt",]
 curry_3ptr_1$id <- 1
-# create an age of the shots from the curry
+
+# for the bild time
 curry_3ptr_1$shot_age <- get_shot_age(curry_3ptr_1)
 
-# this takes some time  
+# this takes some time with the random intercepts so we ommit  
 bild_1_ind <- bild_model(curry_3ptr_1, "ind")
 bild_1_MC1 <- bild_model(curry_3ptr_1, "MC1")
-bild_2_MC1R <- bild_model(curry_3ptr_2,"MC1R")
+bild_1_MC1R <- bild_model(curry_3ptr_1, "MC1R")
 bild_1_MC2 <- bild_model(curry_3ptr_1, "MC2")
-bild_2_MC2R <- bild_model(curry_3ptr_2,"MC2R")
+bild_1_MC2R <- bild_model(curry_3ptr_1,"MC2R")
 
 summary(bild_1_ind)
 summary(bild_1_MC1)
@@ -106,8 +106,12 @@ bild_2_MC2R <- bild_model(curry_3ptr_2, "MC2R")
 
 summary(bild_2_idd)
 summary(bild_2_MC1)
-summary(bild_2_MC1R)
+# summary(bild_2_MC1R)
 summary(bild_2_MC2)
 summary(bild_2_MC2R)
 
+## dataset three has teams as different ids
+teams <- ball
+teams$id <- as.numeric(as.factor(teams$team))
+teams$shot_age <- get_shot_age(ball)
 # plot(bild_2_MC2, main = "bild_model_2")
