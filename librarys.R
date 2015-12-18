b43f6e0b079150293d5f6b33ebee89ff1d23a975
John Wilshire <john.h.wilshire@gmail.com>
2015-12-18 11:56:13 +1100

    install librarys script

---
+++ librarys.R
@@ -0,0 +1,9 @@
+# installs the required librarys to run the analysis on katana
+
+install.packages("devtools")
+
+install.packages("bild")
+install.packages("plyr")
+
+devtools::install_github("richfitz/storr")
+devtools::install_github("richfitz/remake")
