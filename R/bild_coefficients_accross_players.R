8ec128ac7c85e6bc612d249954f042c21f73db2a
John Wilshire <john.h.wilshire@gmail.com>
2016-01-06 11:41:32 +1100

    extract coeffs and plot them

--- R/bild_coefficients_accross_players.R
+++ R/bild_coefficients_accross_players.R
@@ -8,13 +8,7 @@ ball <- read_bball_data("data/basketball_shots_curry.csv")
 ball <- data_prepare(ball)
 ball <- unique(ball)
 
-
-get_bild_model_for_player <- function (data, player_id, model_dependence){
-  player_data <- ball[ball$player.player_id == player_id,]
-  return(bild_model(player_data, dependence = model_dependence, summary = FALSE))
-}
-
-get_coefficients_of_models <- function (ball, model_dependence){
+get_all_player_models <- function (ball, model_dependence){
   # we exclude ids that have not taken more than 3 events
   rles_of_ids <- rle(sort(ball$id))
   many <- rles_of_ids$values[rles_of_ids$lengths > 3]
@@ -29,7 +23,7 @@ get_coefficients_of_models <- function (ball, model_dependence){
   # we exclude the players that do not have more than two ids associated with them
   # I was getting some weird error and this fixed it by increasing from 2 (not sure why)
   players <- lapply(players, function (player_id){
-    if(length(unique(small_ball$id[small_ball$player.player_id == player_id])) < 3){
+    if(length(unique(small_ball$id[small_ball$player.player_id == player_id])) < 4){
       return(NULL)
     }
     return(player_id)
@@ -39,12 +33,35 @@ get_coefficients_of_models <- function (ball, model_dependence){
   
   # bild models
   return(
+    # I tried making this mclapply but I kept getting some error 
     lapply(players, function (player){
       print(toString(player))
       player_data <- small_ball[small_ball$player.player_id == player,]
-      print(player_data[1,])
-      return(bild_model(player_data, dependence = model_dependence, summary = FALSE))
+      return(bild_model(player_data, model_dependence, summary = TRUE))
   }))
 }
 
-models <- get_coefficients_of_models(ball, "ind")
+get_coefficients_of_models <- function (all_models){
+  as.data.frame(t(as.data.frame(
+    lapply(all_models, function (m){
+      return(m@coefficients)
+  }))))
+}
+
+# I run into an issue about the 
+# computationally singular: reciprocal condition number when I run it with mc1r
+mc1_models <- get_all_player_models(ball, "MC1")
+
+coeffs_mc1 <- get_coefficients_of_models(mc1_models) 
+
+plot_histograms <- function (coeffs){
+  for(i in 1:ncol(coeffs)){
+    print(
+      qplot(
+        coeffs[,i],
+        xlab = colnames(coeffs)[i]
+      )
+    )
+  }
+}
+plot_histograms(coeffs_mc1)
